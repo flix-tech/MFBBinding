22dbd921dea06488a4b9570d439cd46e5d3506f8
Nikolay Kasyanov <nikolay.kasyanov@flixbus.com>
2017-12-14 12:26:13 +0100

    1 red

--- Tests/MFBBindingTests.m
+++ Tests/MFBBindingTests.m
@@ -758,6 +758,45 @@
     }
 }
 
+#pragma mark - UITextView Binding Test Methods
+
+- (void)test_reverseBindingToUITextViewObject_UpdatesBoundPropertyOnTextChange
+{
+    UITextView *textView = [UITextView new];
+
+    _configuration.secondObject = textView;
+    _configuration.secondKeyPath = NSStringFromSelector(@selector(text));
+
+    _configuration.twoWay = YES;
+
+    [_configuration setUpBinding];
+
+    for (NSString *value in @[ @"Hello World", @"I am", @"a horse" ]) {
+
+        UITextRange *textRange = [textView textRangeFromPosition:textView.beginningOfDocument toPosition:textView.endOfDocument];
+        [textView replaceRange:textRange withText:value];
+        XCTAssertEqualObjects(_objectA.propertyA, value);
+    }
+}
+
+- (void)test_bindingToUITextViewObject_UpdatesBoundPropertyOnTextChange
+{
+    UITextView *textView = [UITextView new];
+
+    _configuration.firstObject = textView;
+    _configuration.firstKeyPath = NSStringFromSelector(@selector(text));
+
+    [_configuration setUpBinding];
+
+    for (NSString *value in @[ @"Hello World", @"I am", @"a horse" ]) {
+
+        UITextRange *textRange = [textView textRangeFromPosition:textView.beginningOfDocument toPosition:textView.endOfDocument];
+        [textView replaceRange:textRange withText:value];
+        XCTAssertEqualObjects(textView.text, value);
+        XCTAssertEqualObjects(_objectB.propertyB, value);
+    }
+}
+
 #ifdef DEBUG
 
 - (void)test_SecondObjectDeallocation_BindingToUIControlObject_HandledCorrectly
